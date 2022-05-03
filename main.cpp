e38484698c434ae138c1a619df79ea78a68450dc
sanketlokhandegithub <63179270+sanketlokhandegithub@users.noreply.github.com>
2022-05-02 20:25:06 -0700

    Adding the main for reference

--- main.cpp
+++ main.cpp
@@ -5,24 +5,27 @@ int main(void)
 	printf("MPU6050 Driver Starting\n");
 
 	MPU6050_RaspbPi* mpu = new MPU6050_RaspbPi(MPU6050_DEVICE_ADDRESS);
-	//MPU6050_RaspbPi* obj = mpu;
+	MPU6050_RaspbPi* obj = mpu; 
 	double* accelerometer = new double[3];
 	double* gyrometer = new double[3];
-	double temperature;
+	double* temperature = new double[1];
+	
+	if (obj->initialize() < 0) return -1;
 
 	for (int i = 0; i < 200; i++)
 	{
-		if (mpu->getDoubleSensorValues(accelerometer, gyrometer, temperature) > 0)
+		sleep(1);
+		if (obj->getDoubleSensorValues(accelerometer, gyrometer, temperature) > 0)
 		{
 			printf("\nAx : %.2f | Ay : %.2f	| Az : %.2f	| Tp : %.2f	| Gx : %.2f	| Gy : %.2f	| Gz: %.2f	\n"
-																											, accelerometer[0]
-																											, accelerometer[1]
-																											, accelerometer[2]
-																											, temperature
-																											, gyrometer[0]
-																											, gyrometer[1]
-																											, gyrometer[2]
-																											  );
+																			, accelerometer[0]
+																			, accelerometer[1]
+																			, accelerometer[2]
+																			, temperature[0]
+																			, gyrometer[0]
+																			, gyrometer[1]
+																			, gyrometer[2]
+																				);
 		}
 	}
 
